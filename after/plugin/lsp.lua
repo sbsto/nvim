91b6db0bf339ec607b7aae8a584776ebd8dc2fef
Sam Brownstone <sbrownstone@moonpay.com>
2023-11-18 21:04:16 +0000

    Fix LSP

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -29,6 +29,14 @@ lsp.on_attach(function(_, bufnr)
     lsp.default_keymaps({ buffer = bufnr })
 
     vim.keymap.set("n", "<leader>ra", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "K" , function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "<leader>gi", function() vim.lsp.buf.implementation() end, opts)
+    vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>ds", function() vim.lsp.diagnostic.show_line_diagnostics() end, opts)
+    vim.keymap.set("n", "<leader>dl", function() vim.lsp.diagnostic.set_loclist() end, opts)
+    vim.keymap.set("n", "<leader>d", function() vim.lsp.buf.open_float() end, opts)
 end)
 
 lsp.setup()
@@ -37,9 +45,11 @@ vim.diagnostic.config({
     virtual_text = true
 })
 
+require('lspconfig').tsserver.setup({})
+
 require('mason').setup({})
 require('mason-lspconfig').setup({
-    ensure_installed = {},
+    ensure_installed = { 'tsserver', 'eslint' },
     handlers = {
         lsp.default_setup,
     },

693325d67bc1ecb62932f72e6e3e31c337cb03f4
Sam Brownstone <sbrownstone@moonpay.com>
2023-11-20 20:39:39 +0000

    Move to formatter.nvim for formatting

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -23,52 +23,12 @@ lsp.set_preferences({
     }
 })
 
-local function format()
-    local filetype = vim.bo.filetype
-    local prettier_filetypes = {
-        "javascript", "typescript", "css", "json", "yaml", "html", "markdown", "vue",
-        "typescriptreact", "javascriptreact"
-    }
-
-    local bufnr = vim.api.nvim_get_current_buf()
-
-    if vim.tbl_contains(prettier_filetypes, filetype) then
-        local local_prettier = vim.fn.getcwd() .. '/node_modules/.bin/prettier'
-        local prettier_cmd = vim.fn.executable(local_prettier) == 1 and local_prettier or
-            (vim.fn.executable('prettier') == 1 and 'prettier')
-
-        if not prettier_cmd then
-            vim.lsp.buf.format({ bufnr = bufnr })
-            return
-        end
-
-        local filename = vim.fn.expand('%:p')
-        local cmd = prettier_cmd .. ' ' .. filename
-
-        vim.fn.jobstart(cmd, {
-            stdout_buffered = true,
-            on_stdout = function(_, data)
-                if data then
-                    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, data)
-                end
-            end,
-            on_exit = function(_, exit_code)
-                if exit_code ~= 0 then
-                    print("Prettier failed to format the file")
-                end
-            end,
-        })
-    else
-        vim.lsp.buf.format({ bufnr = bufnr })
-    end
-end
-
 lsp.on_attach(function(_, bufnr)
     local opts = { buffer = bufnr, remap = false }
 
     lsp.default_keymaps({ buffer = bufnr })
 
-    vim.keymap.set("n", "<leader>fm", function() format() end, opts)
+    vim.api.nvim_set_keymap('n', '<leader>fm', ':Format<CR>', { noremap = true, silent = true })
     vim.keymap.set("n", "<leader>ra", function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
--- lua/sbsto/lazy.lua
+++ lua/sbsto/lazy.lua
@@ -1,84 +1,120 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-    {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.4',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    {
-        'rose-pine/neovim',
-        as = 'rose-pine',
-        config = function()
-            vim.cmd('colorscheme rose-pine')
-        end
-    },
-    {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
-    },
-    {
-        'nvim-treesitter/playground'
-    },
-    {
-        'theprimeagen/harpoon'
-    },
-    { 'VonHeikemen/lsp-zero.nvim',        branch = 'v3.x' },
-    { 'neovim/nvim-lspconfig' },
-    { 'williamboman/mason.nvim' },
-    { 'williamboman/mason-lspconfig.nvim' },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.4",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"rose-pine/neovim",
+		as = "rose-pine",
+		config = function()
+			vim.cmd("colorscheme rose-pine")
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	},
+	{
+		"nvim-treesitter/playground",
+	},
+	{
+		"theprimeagen/harpoon",
+	},
+	{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
+	{ "neovim/nvim-lspconfig" },
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
 
-    -- Autocompletion
-    { 'hrsh7th/nvim-cmp' },
-    { 'hrsh7th/cmp-buffer' },
-    { 'hrsh7th/cmp-path' },
-    { 'saadparwaiz1/cmp_luasnip' },
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'hrsh7th/cmp-nvim-lua' },
+	-- Autocompletion
+	{ "hrsh7th/nvim-cmp" },
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-path" },
+	{ "saadparwaiz1/cmp_luasnip" },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/cmp-nvim-lua" },
 
-    -- Snippets
-    { 'L3MON4D3/LuaSnip' },
-    { 'rafamadriz/friendly-snippets' },
+	-- Snippets
+	{ "L3MON4D3/LuaSnip" },
+	{ "rafamadriz/friendly-snippets" },
 
-    -- Copilot
-    { 'github/copilot.vim' },
+	-- Copilot
+	{ "github/copilot.vim" },
 
-    -- nvim tree
-    {
-        "nvim-tree/nvim-tree.lua",
-        version = "*",
-        lazy = false,
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-        },
-        config = function()
-            local api = require "nvim-tree.api"
+	-- nvim tree
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			local api = require("nvim-tree.api")
 
-            local function opts(desc)
-                return { desc = "nvim-tree: " .. desc, noremap = true, silent = true, nowait = true }
-            end
+			local function opts(desc)
+				return { desc = "nvim-tree: " .. desc, noremap = true, silent = true, nowait = true }
+			end
 
-            -- custom mappings
-            vim.keymap.set('n', '<leader>tt', api.tree.toggle, opts('Toggle'))
+			-- custom mappings
+			vim.keymap.set("n", "<leader>tt", api.tree.toggle, opts("Toggle"))
 
-            vim.keymap.set('n', '<leader>e', api.tree.focus, opts('Toggle'))
+			vim.keymap.set("n", "<leader>e", api.tree.focus, opts("Toggle"))
 
-            require("nvim-tree").setup(
-                require('sbsto.configs.nvimtree')
-            )
-        end,
-    }
+			require("nvim-tree").setup(require("sbsto.configs.nvimtree"))
+		end,
+	},
+	{
+		"mhartington/formatter.nvim",
+		lazy = false,
+		config = function()
+			print("running formatter config")
+			require("formatter").setup({
+				filetype = {
+					typescript = {
+						require("formatter.filetypes.typescript").prettier,
+					},
+					typescriptreact = {
+						require("formatter.filetypes.typescriptreact").prettier,
+					},
+					javascript = {
+						require("formatter.filetypes.javascript").prettier,
+					},
+                    javascriptreact = {
+                        require("formatter.filetypes.javascriptreact").prettier,
+                    },
+                    css = {
+                        require("formatter.filetypes.css").prettier,
+                    },
+                    html = {
+                        require("formatter.filetypes.html").prettier,
+                    },
+                    yaml = {
+                        require("formatter.filetypes.yaml").prettier,
+                    },
+                    json = {
+                        require("formatter.filetypes.json").prettier,
+                    },
+					lua = {
+						require("formatter.filetypes.lua").stylua,
+					},
+				},
+			})
+		end,
+	},
 }
 
 require("lazy").setup(plugins)

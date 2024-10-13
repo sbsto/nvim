f8f7fb76fe83056ce758b45e223e09fdfa37650a
sbsto <brownstone@hey.com>
2024-10-13 01:32:45 +0100

    Remove nvim tree

--- lua/sbsto/lazy.lua
+++ lua/sbsto/lazy.lua
@@ -60,29 +60,12 @@ local plugins = {
 	-- Copilot
 	{ "github/copilot.vim" },
 
-	-- nvim tree
-	{
-		"nvim-tree/nvim-tree.lua",
-		version = "*",
-		lazy = false,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			local api = require("nvim-tree.api")
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  },
 
-			local function opts(desc)
-				return { desc = "nvim-tree: " .. desc, noremap = true, silent = true, nowait = true }
-			end
-
-			-- custom mappings
-			vim.keymap.set("n", "<leader>tt", api.tree.toggle, opts("Toggle"))
-
-			vim.keymap.set("n", "<leader>e", api.tree.focus, opts("Toggle"))
-
-			require("nvim-tree").setup(require("sbsto.configs.nvimtree"))
-		end,
-	},
 	{
 		"mhartington/formatter.nvim",
 		lazy = false,
@@ -91,6 +74,7 @@ local plugins = {
 				filetype = {
 					typescript = select_formatter("typescript"),
 					typescriptreact = select_formatter("typescriptreact"),
+
 					svelte = select_formatter("svelte"),
 					javascript = select_formatter("javascript"),
 					javascriptreact = select_formatter("javascriptreact"),
@@ -104,6 +88,12 @@ local plugins = {
 					rust = {
 						require("formatter.filetypes.rust").rustfmt,
 					},
+          go = {
+            require("formatter.filetypes.go").gofumpt,
+          },
+          nix = {
+            require("formatter.filetypes.nix").nixpkgs_fmt,
+          },
 				},
 			})
 		end,
@@ -114,51 +104,6 @@ local plugins = {
 			-- add any options here
 		},
 		lazy = false,
-	},
-	{
-		"yetone/avante.nvim",
-		event = "VeryLazy",
-		lazy = false,
-		version = false, -- set this if you want to always pull the latest change
-		opts = {
-			-- add any opts here
-		},
-		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-		build = "make",
-		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-		dependencies = {
-			"stevearc/dressing.nvim",
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
-			--- The below dependencies are optional,
-			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-			"zbirenbaum/copilot.lua", -- for providers='copilot'
-			{
-				-- support for image pasting
-				"HakonHarnes/img-clip.nvim",
-				event = "VeryLazy",
-				opts = {
-					-- recommended settings
-					default = {
-						embed_image_as_base64 = false,
-						prompt_for_file_name = false,
-						drag_and_drop = {
-							insert_mode = true,
-						},
-						-- required for Windows users
-						use_absolute_path = true,
-					},
-				},
-			},
-			{
-				-- Make sure to set this up properly if you have lazy=true
-				"MeanderingProgrammer/render-markdown.nvim",
-				opts = {
-					file_types = { "markdown", "Avante" },
-				},
-				ft = { "markdown", "Avante" },
-			},
-		},
 	},
 	{
 		"nvim-lualine/lualine.nvim",

75eeb908e0557bab66a1279341831d77a50eb6fc
Sam Brownstone <sbrownstone@moonpay.com>
2025-03-12 11:07:16 +0000

    Add avante

--- lua/sbsto/lazy.lua
+++ lua/sbsto/lazy.lua
@@ -189,6 +189,51 @@ local plugins = {
 			})
 		end,
 	},
+	{
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		version = "*",
+		opts = {
+			provider = "claude",
+			claude = {
+				endpoint = "https://api.anthropic.com",
+				model = "claude-3-7-sonnet-20250219",
+				temperature = 0,
+				max_tokens = 4096,
+			},
+		},
+		build = "make",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below dependencies are optional,
+			"echasnovski/mini.pick",
+			"nvim-telescope/telescope.nvim",
+			"hrsh7th/nvim-cmp",
+			{
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+					},
+				},
+			},
+			{
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
+		},
+	},
 }
 
 require("lazy").setup(plugins)
